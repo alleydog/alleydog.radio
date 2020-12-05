cfa0cb4ba25aae12b5d0f45f8ea7b64c88e19131
alleydog <51403105+alleydog@users.noreply.github.com>
2020-12-05 10:50:39 +0300

    Update mradio.ru.sh

--- mradio.ru.sh
+++ mradio.ru.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mpv $(curl -sX GET http://mradio.ru/player.html | grep '<audio id="player" src="http://stream2.loveradio.ru:80/16_mradio_24?type=.aac' | awk -F\" '{ print $4 }')
+echo "#EXTM3U8" > ./mradio.ru.m3u8
+echo "#EXTINF:-1,mradio.ru" >> ./mradio.ru.m3u8
+echo $(curl -sX GET http://mradio.ru/player.html | grep '<audio id="player" src="http://stream2.loveradio.ru:80/16_mradio_24?type=.aac' | awk -F\" '{ print $4 }') >> ./mradio.ru.m3u8
