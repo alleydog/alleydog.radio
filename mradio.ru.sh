a95ea5218be340c1f3abd061c0ac8475b5810cdc
alleydog <51403105+alleydog@users.noreply.github.com>
2021-01-04 16:35:42 +0300

    Delete mradio.ru.sh

--- mradio.ru.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "#EXTM3U8" > ./mradio.ru.m3u8
-echo "#EXTINF:-1,mradio.ru" >> ./mradio.ru.m3u8
-echo $(curl -sX GET http://mradio.ru/player.html | grep '<input id="kbps64" type="radio" name="kbps" value="http://stream2.loveradio.ru:80/16_mradio_56?type=.aac' | awk -F\" '{ print $8 }') >> ./mradio.ru.m3u8
