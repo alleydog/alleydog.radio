4c51df47bc67bf34a24db00e6ccf717e20214a59
alleydog <51403105+alleydog@users.noreply.github.com>
2021-01-05 22:08:38 +0300

    Update mradio.ru.sh

--- mradio.ru.sh
+++ mradio.ru.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "#EXTM3U8" > ~/Radio/mradio.ru.m3u8
-echo "#EXTINF:-1,mradio.ru" >> ~/Radio/mradio.ru.m3u8
+echo "#EXTM3U8" > ~/Music/mradio.ru.m3u8
+echo "#EXTINF:-1,mradio.ru" >> ~/Music/mradio.ru.m3u8
 echo $(curl -sX GET http://mradio.ru/player.html | grep '<input id="kbps64" type="radio" name="kbps" value="http://stream2.loveradio.ru:80/16_mradio_56?type=.aac' | awk -F\" '{ print $8 }') >> ~/Radio/mradio.ru.m3u8
-mpv ~/Radio/mradio.ru.m3u8
+mpv ~/Music/mradio.ru.m3u8
