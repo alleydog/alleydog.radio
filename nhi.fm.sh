cc6b79065c6bed0c03a425f633f536bddb97e869
alleydog <51403105+alleydog@users.noreply.github.com>
2021-01-05 22:11:39 +0300

    Update nhi.fm.sh

--- nhi.fm.sh
+++ nhi.fm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "#EXTM3U8" > ~/Radio/nhi.fm.m3u8
-echo "#EXTINF:-1,nhi.fm" >> ~/Radio/nhi.fm.m3u8
-echo "https://s3.radio.co/sb260bd9e3/listen" >> ~/Radio/nhi.fm.m3u8
-mpv ~/Radio/nhi.fm.m3u8
+echo "#EXTM3U8" > ~/Music/nhi.fm.m3u8
+echo "#EXTINF:-1,nhi.fm" >> ~/Music/nhi.fm.m3u8
+echo "https://s3.radio.co/sb260bd9e3/listen" >> ~/Music/nhi.fm.m3u8
+mpv ~/Music/nhi.fm.m3u8
