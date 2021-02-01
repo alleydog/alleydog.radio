a219a6f2267b741277883fc34fbdda297e6934c7
alleydog <alleydo@protonmail.com>
2021-02-01 21:36:36 +0300

    nhi.fm

--- nhi.fm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "#EXTM3U8" > ~/Music/nhi.fm.m3u8
-echo "#EXTINF:-1,nhi.fm" >> ~/Music/nhi.fm.m3u8
-echo "https://s3.radio.co/sb260bd9e3/listen" >> ~/Music/nhi.fm.m3u8
-mpv ~/Music/nhi.fm.m3u8
