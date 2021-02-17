de4db754b7670baf78d3cac9dfdf2e50daa62509
alleydog <alleydog@protonmail.com>
2021-02-17 19:46:11 +0300

    mradio.ru

--- mradio.ru.bat
+++ mradio.ru.bat
@@ -4,6 +4,6 @@ for /f "tokens=2,3" %%I in ('findstr /i "<audio.*></audio>" ^< player.html') do 
 del -y player.html
 for /f "usebackq  delims=" %%i in (`find /n /v "" mradio.tmp ^| find "[1]"`) do (set value=%%i)
 del -y mradio.tmp
-echo #EXTM3U8 > mradio.ru.m3u8
-echo #EXTINF:-1,mradio.ru >> mradio.ru.m3u8
-set /p str3=%value:~7,92% >> mradio.ru.m3u8 < nul
+echo #EXTM3U > mradio.ru.m3u
+echo #EXTINF:-1,mradio.ru >> mradio.ru.m3u
+set /p str3=%value:~7,92% >> mradio.ru.m3u < nul
