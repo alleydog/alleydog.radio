315b3324dfe8e58078118c697e9c9c29f5672b14
alleydog <51403105+alleydog@users.noreply.github.com>
2021-08-23 17:08:59 +0300

    Update roks.ru.bat

--- roks.ru.bat
+++ roks.ru.bat
@@ -4,4 +4,4 @@ echo #EXTINF:-1,roks.ru >> roks.ru.m3u
 curl -sX GET https://roks.ru/player.php | findstr "https://md5-icecast-radiorocks.cdnvideo.ru/roks.stream?md5=" > roks.tmp
 for /f "usebackq  delims=" %%i in (`find /n /v "" roks.tmp ^| find "[1]"`) do (set value=%%i)
 del -y roks.tmp
-set /p str=%value:~14,120% >> roks.ru.m3u < nul
+set /p str=%value:~15,100% >> roks.ru.m3u < nul
